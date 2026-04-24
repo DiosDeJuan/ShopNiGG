24017c145a4f67ea5e45f6f26c2560345b5abf9f
Isaac Victoria <ijuanddvm04@gmail.com>
2026-04-24 14:03:53 -0600

    LOL

--- Assets/StoreSimulator/Scripts/PlayerController.cs
+++ Assets/StoreSimulator/Scripts/PlayerController.cs
@@ -1,7 +1,3 @@
-/*  This file is part of the "Store Simulator" project by FLOBUK.
- *  You are only allowed to use these resources if you've bought them from an official reseller (Unity Asset Store, Epic FAB).
- *  You shall not license, sublicense, sell, resell, transfer, assign, distribute or otherwise make available to any third party the Service or the Content. */
-
 using UnityEngine;
 using UnityEngine.InputSystem;
 
